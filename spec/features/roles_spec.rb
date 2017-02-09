a24312810ea917b9a51b78a5d853ac288e707049
shlok007 <shlok.srivastava15@gmail.com>
2017-02-10 01:41:22 +0530

    added tests for each role

--- spec/features/roles_spec.rb
+++ spec/features/roles_spec.rb
@@ -6,14 +6,16 @@ feature Role do
   let!(:organizer_role) { Role.find_by(name: 'organizer', resource: conference) }
   let!(:organizer) { create(:user, role_ids: [organizer_role.id]) }
 
-  shared_examples 'successfully' do |role_name|
+  shared_examples 'addition of roles successfully' do |role_name, by_role_name|
     let!(:role) { Role.find_by(name: role_name, resource: conference) }
+    let!(:by_role) { Role.find_by(name: by_role_name, resource: conference) }
+    let!(:user_with_by_role) { create(:user, role_ids: [by_role.id]) }
+
     before(:each) do
-      sign_in organizer
+      sign_in user_with_by_role
       visit admin_conference_roles_path(conference.short_title)
     end
-
-    scenario 'add role' do
+    scenario 'adds role' do
       click_link('Users', href: admin_conference_role_path(conference.short_title, role.name))
 
       fill_in 'user_email', with: other_user.email
@@ -23,8 +25,16 @@ feature Role do
 
       expect(other_user.has_role?(role.name, conference)).to eq true
     end
+  end
 
-    scenario 'edit role' do
+  shared_examples 'editing of roles successfully' do |role_name|
+    let!(:role) { Role.find_by(name: role_name, resource: conference) }
+    before(:each) do
+      sign_in organizer
+      visit admin_conference_roles_path(conference.short_title)
+    end
+
+    scenario 'edits role' do
       click_link('Edit', href: edit_admin_conference_role_path(conference.short_title, role.name))
 
       fill_in 'role_description', with: 'changed description'
@@ -37,21 +47,52 @@ feature Role do
     end
   end
 
+  ##
+  #Organizers can manage users for all roles as well as edit all role.
   context 'organizer' do
     describe 'for organizer role' do
-      it_behaves_like 'successfully', 'organizer'
+      it_behaves_like 'addition of roles successfully', 'organizer', 'organizer'
+      it_behaves_like 'editing of roles successfully', 'organizer'
     end
 
     describe 'for volunteers_coordinator role' do
-      it_behaves_like 'successfully', 'volunteers_coordinator'
+      it_behaves_like 'addition of roles successfully', 'volunteers_coordinator', 'organizer'
+      it_behaves_like 'editing of roles successfully', 'volunteers_coordinator'
     end
 
     describe 'for info_desk role' do
-      it_behaves_like 'successfully', 'info_desk'
+      it_behaves_like 'addition of roles successfully', 'info_desk', 'organizer'
+      it_behaves_like 'editing of roles successfully','info_desk'
     end
 
     describe 'for cfp role' do
-      it_behaves_like 'successfully', 'cfp'
+      it_behaves_like 'addition of roles successfully','cfp', 'organizer'
+      it_behaves_like 'editing of roles successfully','cfp'
+    end
+  end
+
+  ##
+  #User with role info_desk can only manage users for info_desk role
+  context 'info_desk' do
+    describe 'for info_desk role' do
+      it_behaves_like 'addition of roles successfully', 'info_desk', 'info_desk'
+    end
+  end
+
+
+  ##
+  #Users with role cfp can only manage users for cfp role
+  context 'cfp' do
+    describe 'for cfp role' do
+      it_behaves_like 'addition of roles successfully', 'cfp', 'cfp'
+    end
+  end
+
+  ##
+  #Users with role volunteers_coordinator can only manage users for volunteers_coordinator role
+  context 'volunteers_coordinator' do
+    describe 'for volunteers_coordinator role' do
+      it_behaves_like 'addition of roles successfully', 'volunteers_coordinator', 'volunteers_coordinator'
     end
   end
 end
