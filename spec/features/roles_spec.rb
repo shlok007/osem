e5cf3713850eed534c45630d0975a14744b445a2
shlok007 <shlok.srivastava15@gmail.com>
2017-02-21 00:44:13 +0530

    multiple changes:

--- spec/features/roles_spec.rb
+++ spec/features/roles_spec.rb
@@ -2,97 +2,71 @@ require 'spec_helper'
 
 feature Role do
   let(:conference) { create(:conference) }
-  let!(:other_user) { create :user }
-  let!(:organizer_role) { Role.find_by(name: 'organizer', resource: conference) }
-  let!(:organizer) { create(:user, role_ids: [organizer_role.id]) }
 
-  shared_examples 'addition of roles successfully' do |role_name, by_role_name|
+  shared_examples 'correctly succeeds or fails to' do |role_name, by_role_name|
     let!(:role) { Role.find_by(name: role_name, resource: conference) }
+    let!(:user_with_role) { create(:user, role_ids: [role.id]) }
     let!(:by_role) { Role.find_by(name: by_role_name, resource: conference) }
-    let!(:user_with_by_role) { create(:user, role_ids: [by_role.id]) }
+    let!(:user_to_sign_in) { create(:user, role_ids: [by_role.id]) }
+    let!(:user_with_no_role) { create :user }
+    let!(:expected_role_names) { Role.all.each.map(&:name).reject { |name| name == role_name } }
 
-    before(:each) do
-      sign_in user_with_by_role
-      visit admin_conference_roles_path(conference.short_title)
-    end
-    scenario 'adds role' do
-      click_link('Users', href: admin_conference_role_path(conference.short_title, role.name))
-
-      fill_in 'user_email', with: other_user.email
-
-      click_button 'Add'
-      other_user.reload
-
-      expect(other_user.has_role?(role.name, conference)).to eq true
-    end
-  end
-
-  shared_examples 'editing of roles successfully' do |role_name|
-    let!(:role) { Role.find_by(name: role_name, resource: conference) }
-    before(:each) do
-      sign_in organizer
+    before :each do
+      sign_in user_to_sign_in
       visit admin_conference_roles_path(conference.short_title)
     end
 
-    scenario 'edits role' do
-      click_link('Edit', href: edit_admin_conference_role_path(conference.short_title, role.name))
+    scenario 'adds role', feature: true, js: true do
+      click_link('Users', href: admin_conference_role_path(conference.short_title, role_name))
 
-      fill_in 'role_description', with: 'changed description'
+      if page.has_field?('user_email')
+        fill_in 'user_email', with: user_with_no_role.email
+        click_button 'Add'
+        user_with_no_role.reload
 
-      click_button 'Update Role'
-      role.reload
+        expect(user_with_no_role.has_role?(role.name, conference)).to eq true
+        expect(by_role_name).to eq(role_name) | eq('organizer')
+      else
+        expect(by_role_name).to be_in(expected_role_names)
+      end
+    end
 
-      expect(flash).to eq("Successfully updated role #{role.name}")
-      expect(role.description).to eq('changed description')
+    scenario 'edits role', feature: true, js: true do
+      if page.has_link?('Edit')
+        click_link('Edit', href: edit_admin_conference_role_path(conference.short_title, role_name))
+        fill_in 'role_description', with: 'changed description'
+        click_button 'Update Role'
+        role.reload
+
+        expect(flash).to eq("Successfully updated role #{role_name}")
+        expect(role.description).to eq('changed description')
+        expect(by_role_name).to eq 'organizer'
+      else
+        expect(by_role_name).not_to eq 'organizer'
+      end
+    end
+
+    scenario 'removes role', feature: true, js: true do
+      click_link('Users', href: admin_conference_role_path(conference.short_title, role_name))
+
+      if first('td').has_css?('.bootstrap-switch-container')
+        bootstrap_switch = first('td').find('.bootstrap-switch-container')
+        bootstrap_switch.click
+
+        expect(find('.alert').text).to eq "×Successfully removed role #{role_name} from user #{user_with_role.email}"
+        expect(by_role_name).to eq(role_name) | eq('organizer')
+        expect(user_with_role.has_role?(role_name, conference)).to eq false
+      else
+        expect(by_role_name).to be_in(expected_role_names)
+      end
     end
   end
 
-  ##
-  #Organizers can manage users for all roles as well as edit all role.
-  context 'organizer' do
-    describe 'for organizer role' do
-      it_behaves_like 'addition of roles successfully', 'organizer', 'organizer'
-      it_behaves_like 'editing of roles successfully', 'organizer'
-    end
-
-    describe 'for volunteers_coordinator role' do
-      it_behaves_like 'addition of roles successfully', 'volunteers_coordinator', 'organizer'
-      it_behaves_like 'editing of roles successfully', 'volunteers_coordinator'
-    end
-
-    describe 'for info_desk role' do
-      it_behaves_like 'addition of roles successfully', 'info_desk', 'organizer'
-      it_behaves_like 'editing of roles successfully','info_desk'
-    end
-
-    describe 'for cfp role' do
-      it_behaves_like 'addition of roles successfully','cfp', 'organizer'
-      it_behaves_like 'editing of roles successfully','cfp'
-    end
-  end
-
-  ##
-  #User with role info_desk can only manage users for info_desk role
-  context 'info_desk' do
-    describe 'for info_desk role' do
-      it_behaves_like 'addition of roles successfully', 'info_desk', 'info_desk'
-    end
-  end
-
-
-  ##
-  #Users with role cfp can only manage users for cfp role
-  context 'cfp' do
-    describe 'for cfp role' do
-      it_behaves_like 'addition of roles successfully', 'cfp', 'cfp'
-    end
-  end
-
-  ##
-  #Users with role volunteers_coordinator can only manage users for volunteers_coordinator role
-  context 'volunteers_coordinator' do
-    describe 'for volunteers_coordinator role' do
-      it_behaves_like 'addition of roles successfully', 'volunteers_coordinator', 'volunteers_coordinator'
+  Role.all.each.map(&:name).repeated_permutation(2) do |role, by_role|
+    context by_role do
+      describe "for #{role} role" do
+        it_behaves_like 'correctly succeeds or fails to', role, by_role
+      end
     end
   end
 end
